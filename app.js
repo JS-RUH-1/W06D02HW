bbd95312fad2386ba0b099e698c62d320c528835
TheRealBasel <alanaziibasel@gmail.com>
2021-11-08 14:45:15 +0300

    Fix some issues & add PUT & DELETE methods

--- app.js
+++ app.js
@@ -10,11 +10,20 @@ app.get('/games',function(request,response){
     response.send(jsonObject); 
 });
 
+function getFreeGameNumber (){
+    for ( let i = 1; i < 50; i++ ){
+        if ( !jsonObject[`Game${i}`] ){
+            return i;
+        }
+    }
+}
+
 app.post('/games',function(request,response){
     let jsonLength = Object.keys(jsonObject).length
+    let freeGameID = getFreeGameNumber();
     if (request.body && request.body.name && request.body.name != "") {
         let gameObject = {
-            ["Game"+(jsonLength+1)]: {
+            ["Game" + freeGameID]: {
                 id: jsonLength + 1,
                 name: request.body.name
             }
@@ -33,37 +42,29 @@ app.post('/games',function(request,response){
     }
 });
 
-app.post('/gamesDelete',function(request,response){
+app.delete('/gamesDelete',function(request,response){
     if (jsonObject["Game"+request.body.id]){
-        if (request.body && request.body.id && request.body.id != "") {
-            delete jsonObject["Game"+request.body.id];
-            fileHandler.writeFile ( './json/games.json', JSON.stringify(jsonObject,null, '\t'), (error) => 
-                {
-                    if ( error ) throw error; 
-                    response.send(jsonObject); 
-                }
-            )
-        }else{
-            response.send({"message":"Error"}); 
-        }
+        delete jsonObject["Game"+request.body.id];
+        fileHandler.writeFile ( './json/games.json', JSON.stringify(jsonObject,null, '\t'), (error) => 
+            {
+                if ( error ) throw error; 
+                response.send(jsonObject); 
+            }
+        )
     }else{
         response.send({"message":"Game not found"}); 
     }
 });
 
-app.post('/gamesEdit',function(request,response){
+app.put('/gamesEdit',function(request,response){
     if (jsonObject["Game"+request.body.id]){
-        if (request.body && request.body.id && request.body.id != "" && request.body.name && request.body.name != "") {
-            jsonObject["Game"+request.body.id].name = request.body.name
-            fileHandler.writeFile ( './json/games.json', JSON.stringify(jsonObject,null, '\t'), (error) => 
-                {
-                    if ( error ) throw error; 
-                    response.send(jsonObject["Game"+request.body.id]); 
-                }
-            )
-        }else{
-            response.send({"message":"Error"}); 
-        }
+        jsonObject["Game"+request.body.id].name = request.body.name
+        fileHandler.writeFile ( './json/games.json', JSON.stringify(jsonObject,null, '\t'), (error) => 
+            {
+                if ( error ) throw error; 
+                response.send(jsonObject["Game"+request.body.id]); 
+            }
+        )
     }else{
         response.send({"message":"Game not found"}); 
     }
