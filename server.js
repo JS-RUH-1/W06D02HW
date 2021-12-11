6f1a4acd6e05462a9d0e9715864ab18e645841f9
amjadT1000 <Amjad.Athathi.ruh.js@tuwaiq.edu.sa>
2021-12-11 17:05:46 +0300

    DONE

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const app = express()
+const fileHandler = require('fs');
+let jsonData = fileHandler.readFileSync('./json/games.json');
+let jsonObject = JSON.parse(jsonData);
+
+app.use(express.json())
+  
+app.get('/games',function(request,response){
+    response.send(jsonObject); 
+});
+
+function getFreeGameNumber (){
+    for ( let i = 1; i < 50; i++ ){
+        if ( !jsonObject[`Game${i}`] ){
+            return i;
+        }
+    }
+}
+
+app.post('/games',function(request,response){
+    let jsonLength = Object.keys(jsonObject).length
+    let freeGameID = getFreeGameNumber();
+    if (request.body && request.body.name && request.body.name != "") {
+        let gameObject = {
+            ["Game" + freeGameID]: {
+                id: jsonLength + 1,
+                name: request.body.name
+            }
+        }
+        if ( gameObject ){
+            Object.assign(jsonObject,gameObject);
+                /////////
+            fileHandler.writeFile ( './json/games.json', JSON.stringify(jsonObject,null, '\t'), (error) => 
+                {
+                    if ( error ) throw error; 
+                    response.send(gameObject); 
+                }
+            )
+            /////
+        }
+    }else{
+        response.send({"message":"Error"}); 
+    }
+});
+
+app.delete('/gamesDelete/:id',function(request,response){
+    if (jsonObject["Game"+request.params.id]){
+        delete jsonObject["Game"+request.params.id];
+        fileHandler.writeFile ( './json/games.json', JSON.stringify(jsonObject,null, '\t'), (error) => 
+            {
+                if ( error ) throw error; 
+                response.send(jsonObject); 
+            }
+        )
+    }else{
+        response.send({"message":"Game not found"}); 
+    }
+});
+
+app.put('/gamesEdit/:id',function(request,response){
+    if (jsonObject["Game"+request.params.id]){
+        jsonObject["Game"+request.params.id] = request.body
+        fileHandler.writeFile ( './json/games.json', JSON.stringify(jsonObject,null, '\t'), (error) => 
+            {
+                if ( error ) throw error; 
+                response.send(jsonObject["Game"+request.params.id]); 
+            }
+        )
+    }else{
+        response.send({"message":"Game not found"}); 
+    }
+});
+
+app.listen(8080, () => {
+  console.log(`Example app listening at http://localhost:8080`)
+})
